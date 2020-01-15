022aae2a1f1f7e92be5cb99a577c7f9e1b3deee4
abeUzi <ibanez1111.music@gmail.com>
2020-01-15 10:54:26 +0900

    [add] action,reducerの追加
    ・addTodo
    Todoを追加する為のアクション。
    
    ・completeTodo
    Todoを完了/未完了の状態に変更するアクション。

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,27 @@
 import {combineReducers} from 'redux';
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false,
+        },
+      ];
+    case 'COMPLETE_TODO':
+      state[action.id].completed = true;
+      const newState = [].concat(state);
+      return newState;
+    // return state.map(todo =>
+    //   todo.id === action.id ? {...todo, completed: !todo.completed} : todo
+    // );
+    default:
+      return state;
+  }
+};
 
-export default combineReducers({});
+export default combineReducers({
+  todos,
+});
